1f5f05f67dfa7bab35be48de86b4cd2d037c50f4
qsy <434772544@qq.com>
2020-12-10 10:15:23 +0800

    1111

--- src/main/java/com/test/controller/TestController.java
+++ src/main/java/com/test/controller/TestController.java
@@ -1,7 +1,5 @@
 package com.test.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,14 +10,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TestController {
 
 
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
-
-
     @GetMapping
     @ResponseBody
     public String a(){
-        stringRedisTemplate.opsForValue().set(String.valueOf(Math.random()),"123");
         return "success";
     }
 }
